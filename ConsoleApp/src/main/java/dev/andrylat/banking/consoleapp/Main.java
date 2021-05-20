34525a6fc932cfc6502ed250c755e8d943271523
Nikitosique <81965440+Nikitosique@users.noreply.github.com>
2021-05-20 15:28:05 +0300

    Update Main.java

--- ConsoleApp/src/main/java/dev/andrylat/banking/consoleapp/Main.java
+++ ConsoleApp/src/main/java/dev/andrylat/banking/consoleapp/Main.java
@@ -2,13 +2,9 @@ package dev.andrylat.banking.consoleapp;
 
 import dev.andrylat.banking.consoleapp.menu.Menu;
 
-import java.util.Locale;
-
 public class Main {
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.FRANCE);
-
         Menu menu = new Menu();
         menu.chooseOperation();
     }
